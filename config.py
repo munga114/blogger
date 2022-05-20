4bfe62e8b6402bdd6359a66964d54c2911ffac9b
munga114 <mungai.mbugua@student.moringaschool.com>
2022-05-20 19:05:37 +0300

    work on migrations

--- config.py
+++ config.py
@@ -1,25 +1,19 @@
-from  dotenv import load_dotenv
-load_dotenv()
 import os
+
+
 class Config:
-       
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    '''
-    General configuration parent class
-    '''
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mungai:5458@localhost/blogz'
-    SECRET_KEY='12345'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mungai:gaza@localhost/ip4'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    MAIL_SERVER = 'smtp.gmail.com'
+    SESSION_COOKIE_SECURE = False
+    SECRET_KEY='12345'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+    WTF_CSRF_ENABLED = False
 
 
 class ProdConfig(Config):
@@ -28,18 +22,9 @@ class ProdConfig(Config):
         SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",)
 
 
-
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL")
-    
-    DEBUG = True
-
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mungai:gaza@localhost/ip4'
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export SECRET_KEY='12345'
-export DATABASE_URL= 'postgresql+psycopg2://mungai:5458@localhost/blogz'
+export DATABASE_URL= 'postgresql+psycopg2://mungai:gaza@localhost/ip4'
 python3.8 manage.py server
